0ebb45cd20362dba9ec8d5a635ff2e4d8075717e
sgorskyy <serge.gorskyy@gmail.com>
2023-01-19 09:30:12 +0200

    homework2

---
+++ src/com/ua/main/homework3/Main.java
@@ -0,0 +1,18 @@
+package com.ua.main.homework3;
+
+public class Main {
+    public static void main(String[] args) {
+        int i1 = 20;
+        int i2 = 10;
+
+        System.out.println("i1 = "+i1+",i2 = "+i2);
+        System.out.print("i1+i2 = ");
+        System.out.println(i1+i2);
+        System.out.print("i1-i2 = ");
+        System.out.println(i1-i2);
+        System.out.print("i1/i2 = ");
+        System.out.println(i1/i2);
+        System.out.print("i1*i2 = ");
+        System.out.println(i1*i2);
+    }
+}
