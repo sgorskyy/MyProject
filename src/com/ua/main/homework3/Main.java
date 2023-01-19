e091ddd2b91736c3678b8987ec979648afb9968d
sgorskyy <serge.gorskyy@gmail.com>
2023-01-19 09:32:27 +0200

    homework3

--- src/com/ua/main/homework3/Main.java
+++ src/com/ua/main/homework3/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int i1 = 20;
         int i2 = 10;
-
         System.out.println("i1 = "+i1+",i2 = "+i2);
         System.out.print("i1+i2 = ");
         System.out.println(i1+i2);
