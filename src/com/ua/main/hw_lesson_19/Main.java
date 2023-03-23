f1c5d063dcec9062c512fefbb5ceb66c91f1258c
sgorskyy <serge.gorskyy@gmail.com>
2023-03-23 16:11:04 +0200

    Home work for lesson 19

---
+++ src/com/ua/main/hw_lesson_19/Main.java
@@ -0,0 +1,28 @@
+package com.ua.main.hw_lesson_19;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashSet<Integer> setTestHashSet = new HashSet<>();
+        getRandomSet(setTestHashSet);
+        System.out.println(setTestHashSet);
+
+        Set<Integer> setTestLinkedHashSet = new LinkedHashSet<>();
+        getRandomSet(setTestLinkedHashSet);
+        System.out.println(setTestLinkedHashSet);
+
+        Set<Integer> setTestTreeSet = new TreeSet<>();
+        getRandomSet(setTestTreeSet);
+        System.out.println(setTestTreeSet);
+
+    }
+
+    public static void getRandomSet(Set set) {
+        Random value = new Random();
+        for (int i = 0; i < 1000; i++) {
+            set.add(value.nextInt(50));
+        }
+    }
+}
